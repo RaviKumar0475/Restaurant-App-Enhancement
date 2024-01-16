657db317657d3edc6fd06e6df870a632aa8b2263
RaviKumar <bijjamravi662@gmail.com>
2024-01-16 12:02:09 +0000

    Enhancement-of-Restaurant-App-

---
+++ src/App.js
@@ -0,0 +1,128 @@
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import {Component} from 'react'
+import CartContext from './context/CartContext'
+
+import Login from './components/Login'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+class App extends Component {
+  state = {cartList: []}
+
+  addCartItem = product => {
+    const {cartList} = this.state
+    const productObject = cartList.find(
+      eachCartItem => eachCartItem.dishId === product.dishId,
+    )
+
+    if (productObject) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachCartItem => {
+          if (productObject.dishId === eachCartItem.dishId) {
+            const updatedQuantity =
+              eachCartItem.cartCount[product.dishId] +
+              product.cartCount[product.dishId]
+            return {
+              ...eachCartItem,
+              cartCount: {
+                ...eachCartItem.cartCount,
+                [product.dishId]: updatedQuantity,
+              },
+            }
+          }
+          return eachCartItem
+        }),
+      }))
+    } else {
+      const updatedCartList = [...cartList, product]
+      this.setState({cartList: updatedCartList})
+    }
+
+    console.log(product)
+  }
+
+  removeAllCartItems = () => {
+    this.setState({cartList: []})
+  }
+
+  incrementCartItemQuantity = dishId => {
+    this.setState(prevState => ({
+      cartList: prevState.cartList.map(eachCartItem => {
+        if (dishId === eachCartItem.dishId) {
+          const updatedQuantity =
+            eachCartItem.cartCount[eachCartItem.dishId] + 1
+          return {
+            ...eachCartItem,
+            cartCount: {
+              ...eachCartItem.cartCount,
+              [eachCartItem.dishId]: updatedQuantity,
+            },
+          }
+        }
+        return eachCartItem
+      }),
+    }))
+  }
+
+  decrementCartItemQuantity = dishId => {
+    const {cartList} = this.state
+    const productObject = cartList.find(
+      eachCartItem => eachCartItem.dishId === dishId,
+    )
+    if (productObject.cartCount[dishId] > 1) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachCartItem => {
+          if (dishId === eachCartItem.dishId) {
+            const updatedQuantity = eachCartItem.cartCount[dishId] - 1
+            return {
+              ...eachCartItem,
+              cartCount: {...eachCartItem.cartCount, [dishId]: updatedQuantity},
+            }
+          }
+          return eachCartItem
+        }),
+      }))
+    } else {
+      this.removeCartItem(dishId)
+    }
+  }
+
+  removeCartItem = dishId => {
+    const {cartList} = this.state
+    const updatedCartList = cartList.filter(
+      eachCartItem => eachCartItem.dishId !== dishId,
+    )
+
+    this.setState({cartList: updatedCartList})
+  }
+
+  render() {
+    const {cartList} = this.state
+
+    return (
+      <BrowserRouter>
+        <CartContext.Provider
+          value={{
+            cartList,
+            addCartItem: this.addCartItem,
+            removeCartItem: this.removeCartItem,
+            removeAllCartItems: this.removeAllCartItems,
+            incrementCartItemQuantity: this.incrementCartItemQuantity,
+            decrementCartItemQuantity: this.decrementCartItemQuantity,
+          }}
+        >
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <ProtectedRoute exact path="/" component={Home} />
+            <ProtectedRoute exact path="/cart" component={Cart} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="not-found" />
+          </Switch>
+        </CartContext.Provider>
+      </BrowserRouter>
+    )
+  }
+}
+export default App
